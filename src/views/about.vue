6656b06392e896faf083e45b74da629677691d03
雾山 <li997477295@outlook.com>
2021-07-16 11:38:41 +0800

    Update about.vue

--- src/views/about.vue
+++ src/views/about.vue
@@ -31,8 +31,7 @@
                       <p>有一段时间我发现我的收藏夹越来越多，很难找到某个不常用的网站。
                         所以就有了这个网站，这里收集的基本都是我自己浏览器收藏夹里的网站，我平时用的相对比较多的一些都在这里。</p>
                       <br />
-                      <p>如果你也是设计师，如果你也正好喜欢设计，那希望这个网站能给你带来一些作用。</p>
-                      <p>闲聊或者有任何建议都可以加QQ群交流：304584722</p>
+                      <p>如果你也是前端开发工程师，如果你也正好喜欢前端，那希望这个网站能给你带来一些作用。</p>
                     </blockquote>
                   </div>
                 </div>
@@ -65,13 +64,13 @@
                         <br />
                         <blockquote>
                           <p>
-                            这是一个公益项目，而且是<a href="https://github.com/WebStackPage/WebStackPage.github.io"> 开源 </a>
+                            这是一个公益项目，而且是<a href="https://github.com/linux-jin/WebStack-vue"> 开源 </a>
                             <iframe src="https://ghbtns.com/github-btn.html?user=WebStackPage&repo=WebStackPage.github.io&type=star&count=true"
                                     frameborder="0" scrolling="0" width="100px" height="20px"></iframe> 的。你也可以拿来制作自己的网址导航。如果你有更好的想法，可以通过个人网站
-                            <a href="http://www.viggoz.com">
+                            <a href="https://github.com/beer-on-ice">
                               <span class="label label-info" data-toggle="tooltip"
                                     data-placement="left" title=""
-                                    data-original-title="Hello I am a Tooltip">viggoz.com</span>
+                                    data-original-title="Hello I am a Tooltip">github.com/beer-on-ice</span>
                             </a>中的联系方式找到我，欢迎与我交流分享。
                           </p>
                         </blockquote>
